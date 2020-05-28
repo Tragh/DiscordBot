9c46d076bf13912d715fbe77ff3afe883c2a8171
Tragh <mikemth@gmail.com>
2020-05-29 00:43:54 +0100

    Initial commit

---
+++ bot.py
@@ -0,0 +1,79 @@
+# bot.py
+import os
+import io
+import discord
+import asyncio
+from dotenv import load_dotenv
+from helper_classes import *
+
+botActionController=BotActionController()
+
+
+
+
+async def reportForDuty(message):
+	await message.channel.send("bot reporting for duty!")
+		
+botActionController.createBotAction() \
+	.inChannel('admin-test') \
+	.inChannel('admin-chat') \
+	.onCommand("!bot") \
+	.takeAction(reportForDuty) \
+	.done()
+
+async def repostMessage(message):
+	await asyncio.sleep(2)
+	
+	discordFileList=[] #Here we'll store references to the files so that we can upload them
+	fileBufferList=[] #Here we'll store the file buffers so that we can close them later
+	
+	for attachment in message.attachments:
+		filebuffer=io.BytesIO() #create a file buffer
+		await attachment.save(filebuffer) #save the message's file in the buffer we just made
+		discordFileList.append(discord.File(filebuffer,filename=attachment.filename)) #make this filebuffer into a discord file buffer object and add it to the list
+		fileBufferList.append(filebuffer) #now append the buffer to the filebufferlist so we can close it later
+	
+	if len(discordFileList) == 0:
+		await message.channel.send(content = message.content[11:])
+	else:
+		await message.channel.send(content = message.content[11:], files=discordFileList)
+	
+	for fileBuffer in fileBufferList:
+		fileBuffer.close()
+
+botActionController.createBotAction() \
+	.inChannel('admin-chat') \
+	.onCommand("!botrepost") \
+	.takeAction(repostMessage) \
+	.done()
+
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+	for guild in client.guilds:
+		print(
+			f'{client.user} is connected to the following guild:\n'
+			f'{guild.name}(id: {guild.id})'
+		)
+
+@client.event
+async def on_message(message):
+	if message.author == client.user:
+		return
+	if message.is_system():
+		return
+	
+	for botAction in botActionController.botActionsList:
+		if message.channel.name in botAction.channels and (message.content.startswith(botAction.command + ' ') or message.content == botAction.command):
+			await botAction.action(message)
+	
+	
+	
+
+client.run(TOKEN)
---
+++ helper_classes.py
@@ -0,0 +1,28 @@
+class BotAction:
+	channels=[]
+	command=''
+	def action(message):
+		pass
+		
+class BotActionController:
+	def __init__(self):
+		self.botActionsList=[]
+	def createBotAction(self):
+		return BotActionFactory(self)
+
+class BotActionFactory:
+	def __init__(self,parent):
+		self.botAction=BotAction()
+		self.parent=parent
+	def onCommand(self, command):
+		self.botAction.command=command
+		return self
+	def inChannel(self,channel):
+		if channel not in self.botAction.channels:
+			self.botAction.channels.append(channel)
+		return self
+	def takeAction(self,action):
+		self.botAction.action=action
+		return self
+	def done(self):
+		self.parent.botActionsList.append(self.botAction)
